ab33ccb0617f71786833727f6255274bc1001652
MaxDmytruk <max.dmt@i.ua>
2017-11-11 21:59:30 +0200

    removed unused code

--- src/main/java/models/Customer.java
+++ src/main/java/models/Customer.java
@@ -1,7 +1,5 @@
 package models;
 
-import org.hibernate.validator.constraints.Length;
-
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
